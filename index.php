a8fc140c1183ba5f73d63a1e3974c8f64b9b2df0
abardashevskiy <andrey.bardashevskiy@omnigon.com>
2017-01-30 15:13:09 +0200

    removed unnecessary libxml calls

--- index.php
+++ index.php
@@ -8,12 +8,8 @@
 
 require_once __DIR__ . '/ForkPlayer/fp.php';
 
-libxml_use_internal_errors(true);
-
 use ForkPlayer\ForkPlayer;
 
 $fp = new ForkPlayer();
 
 print $fp->dispatch();
-
-libxml_clear_errors();
